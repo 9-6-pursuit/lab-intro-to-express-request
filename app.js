315d10f33f95aa52aae8ed240214883e682d16d1
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-25 15:17:43 -0400

    ready to submit

--- app.js
+++ app.js
@@ -3,37 +3,44 @@ const express = require("express");
 
 // CONFIGURATION
 const app = express();
+const pokemon = require("./models/pokemon.json");
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Homepage");
+  console.log(pokemon[0]);
+  res.send("Welcome 99 Pokemon");
 });
 
-app.get("/:verb/:adjective/:noun", (req, res) => {
-  res.send(
-    `Congratulations on starting a new project called ${req.params.verb}-${req.params.adjective}-${req.params.noun}.`
-  );
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
 });
 
-app.get("/bugs", (req, res) => {
-  res.send(`<div>
-    <h3>99 little bugs in the code</h3>
-    <a href="bugs/101">Link</a>
-    </div>`);
-});
 
-app.get("/bugs/:number", (req, res) => {
-  const bugsNumber = req.params.number;
-  if (bugsNumber) {
-    res.send(`<div>
-      <h3>${bugsNumber} little bugs in the code</h3>
-      <a href="${Number(bugsNumber) + 2}">Link</a>
-      </div>`);
+app.get("/pokemon/search", (req, res) => {
+    let pokemonName = req.query.name;
+    let targetPokemon = pokemon.find((poke) => {
+      return poke.name.toLowerCase() === pokemonName.toLowerCase();
+    });
+  
+    if (targetPokemon) {
+      res.send(targetPokemon);
+    } else {
+      res.send([]);
+    }
+  });
+  
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const index = req.params.indexOfArray;
+  if (index < pokemon.length) {
+    const selectedPokemon = pokemon[index];
+    if (selectedPokemon) {
+      res.send(selectedPokemon);
+    } else {
+      res.send(`Sorry, no pokemon found at ${index}`);
+    }
   } else {
-    res.send(`<div>
-      <h3>${req.params.numberOfBugs} little bugs in the code</h3>
-      <a href="/bugs">Start Over</a>
-      </div>`);
+    res.send(`Sorry, no pokemon found at ${index}`);
   }
 });
 
@@ -43,12 +50,42 @@ app.get("/bugs/:number", (req, res) => {
 
 
 
-const pokemon = require("./models/pokemon.json");
-console.log(pokemon[0]);
 
 
 
 
+
+
+
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  res.send(
+    `Congratulations on starting a new project called ${req.params.verb}-${req.params.adjective}-${req.params.noun}!`
+  );
+});
+
+app.get("/bugs", (req, res) => {
+  res.send(`<div>
+        <h3>99 little bugs in the code</h3>
+        <a href="bugs/101">pull one down, patch it around</a>
+    </div>`);
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  if (req.params.numberOfBugs <= 200) {
+    res.send(`<div>
+            <h3>${req.params.numberOfBugs} little bugs in the code</h3>
+            <a href="${
+              Number(req.params.numberOfBugs) + 2
+            }">pull one down, patch it around</a>
+        </div>`);
+  } else {
+    res.send(`<div>
+            <h3>${req.params.numberOfBugs} little bugs in the code</h3>
+            <a href="/bugs">Start over</a>
+        </div>`);
+  }
+});
 
 // EXPORT
 module.exports = app;
