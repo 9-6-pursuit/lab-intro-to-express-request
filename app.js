a355b315776d28bead56b49ca8e2537320a37ebe
amshepard <antoineshepard@pursuit.org>
2023-07-02 15:27:12 -0400

    11 passed.

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
-// DEPENDENCIES
-const express = require("express");
+const  express = require("express");
+const app = express()
+const pokemon = require("./models/pokemon.json");
 
-// CONFIGURATION
-const app = express();
+app.get("/", (req, res)=>{
+    res.send("Welcome 99 Pokemon")
+})
 
-// ROUTES
-app.get("/", (req, res) => {
-  res.send("Welcome to the homepage.");
-});
+app.get(("/:verb/:adjective/:noun"), (req, res)=>{
+    const {verb, adjective, noun} = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
 
-// ROUTES
-// app.get("/rocks/:index", (req, res) => {
-//     // res.send(rocks);
-//     // res.send(req.params);
-//     res.send(rocks[req.params.index]);
-//   });
+app.get("/bugs", (req, res)=>{
+    res.send(`<h1>99 little bugs in the code</h1><a href="/bugs/101"> pull one down, patch it around </a>`)
+})
 
-// EXPORT
+app.get("/bugs", (req, res) => {
+    res.send(
+    `<h1>99 little bugs in the code</h1>
+    <a href= "/bugs/101"> Pull one down, patch it around</a>`
+    );
+})
+
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    const { numberOfBugs } = req.params
+    bugsNum = Number(numberOfBugs)
+    if(bugsNum < 200) {
+        res.send(`<h1>"${bugsNum} little bugs in the code"</h1><a href="${bugsNum + 2}">Pull one down, patch it around</a>`)
+    }else{
+        res.send(`<a href="/bugs">Too many bugs!! Start over!</a>`)
+    }
+})
+
+app.get("/pokemon", (req, res)=>{
+    
+    res.send(pokemon)
+})
+
+app.get("/pokemon/search", (req, res)=>{
+    const { name } = req.query;
+    const selectPokemon = pokemon.find((pokemon) => pokemon.name.toLowerCase() === name.toLowerCase())
+    res.send(selectPokemon ? [selectPokemon] : [])
+    
+})
+
+app.get("/pokemon/:index", (req, res)=>{
+    const {index} = req.params;
+    res.send(pokemon[Number(index)] || "Sorry, no pokemon found at " + index)
+})
+
+
+app.get(("*"), (req, res) =>{
+    res.status(404).send("This is not the page you are looking for")
+})
+
 module.exports = app;
