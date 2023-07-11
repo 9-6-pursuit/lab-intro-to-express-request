2e03ed395ff1695beb7451ebce66288996528cd2
“irwinj7” <Irwinjorge@pursuit.com>
2023-07-11 01:20:39 -0400

    finished

--- app.js
+++ app.js
@@ -3,21 +3,61 @@ const express = require("express");
 
 // CONFIGURATION
 const app = express();
+const pokemon = require("./models/pokemon.json");
+
 
 // ROUTES
 app.get("/", (req, res) => {
+  console.log(pokemon[0]);
   res.send("Welcome to Express Name Generator");
 });
 
 app.get("/bugs", (req, res) => {
-    res.send(`<h1>99 little bugs in the code</h1> <a href="/bugs/101">pull one down, patch it around</a>`);
+    res.send(`<div>
+        <h3>99 little bugs in the code</h3> 
+        <a href="/bugs/101">pull one down, patch it around</a>
+    <div>`);
 });
 
-// was working on this one //
 app.get("/bugs/:index", (req, res) => {
     const { index } = req.params
-    res.send(`${index} little bugs in the code <a href="/bugs/:index">pull one down, patch it around</a>`);
+    if (index <= 200) {
+        res.send(`<div>
+            <h3>${index} little bugs in the code </h3> 
+            <a href="${Number(req.params.index) + 2}">pull one down, patch it around</a>
+        </div>`);
+    } else {
+        res.send(`<div>
+            <h3>${index} little bugs in the code </h3> 
+            <a href="/bugs">Start over</a>
+        </div>`);
+    }
 });
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon);
+});
+
+app.get("/pokemon/search", (req, res) => {
+    let pokemonName = req.query.name
+    let targetPokemon = pokemon.find((poke)=>{
+        return poke.name.toLowerCase() === pokemonName
+    })
+    res.send(targetPokemon);
+});
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+    let index = req.params.indexOfArray
+    if (pokemon[index]){
+        res.send(pokemon[index])
+    } else {
+        res.send(`sorry, no pokemon at /pokemon/${index}`)
+    }
+});
+
+
+
+
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
     console.log(req.params);
--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT;
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log(`🪨 Listening on port ${PORT} 💎 `);
+  console.log(`Listening on port ${PORT} 💎 `);
 });
